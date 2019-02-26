e8f185fa5b7daca0011054707e45c666624ca734
Tiffany Do <zyrcant@gmail.com>
2019-02-26 16:54:26 -0600

    changed the look and feel to fit windows

--- ColorCompressorGUI/src/ColorCompressorGUI.java
+++ ColorCompressorGUI/src/ColorCompressorGUI.java
@@ -13,6 +13,8 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -317,15 +319,16 @@ public class ColorCompressorGUI extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ColorCompressorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ColorCompressorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ColorCompressorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(ColorCompressorGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(ColorCompressorGUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        
         //</editor-fold>
 
         /* Create and display the form */
